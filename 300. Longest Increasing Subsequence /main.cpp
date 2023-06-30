08c9f421bbd9f11b3f1dc3a9d606c93099b5712f
Sohaib Manah <souhaibemanah@gmail.com>
2023-06-30 21:44:10 +0100

    #300. Longest Increasing Subsequence

---
+++ 300. Longest Increasing Subsequence /main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+//#include <limits>
+
+
+using namespace std;
+
+class Solution;
+
+
+class Solution {
+    static int max_sub_problems(const vector<int> &L, const vector<int> &nums, const int k) {
+//        int size = int(nums.size());
+        int max_value = 0;
+//        int max_value = numeric_limits<int>::min();// -Infinity
+        for (int i = 0; i < k; ++i) {
+            if (nums[i] < nums[k] && L[i] > max_value) {
+                max_value = L[i];
+            }
+        }
+
+//        cout << "max_value" << max_value << "\n";
+//        for (int i = 0; i < k; ++i) {
+//            cout << "i=" << i << "=>" << L[i] << "\n";
+//        }
+        return max_value;
+//        return max(max_value, 0);
+    }
+
+public:
+
+    static int max_arr(const vector<int> &vec, int length) {
+        int max = 0;
+//        int max = numeric_limits<int>::min();// -Infinity
+        for (int i = 0; i < length; ++i) {
+            if (vec[i] > max)max = vec[i];
+
+        }
+        return max;
+    }
+
+    int lengthOfLIS(vector<int> &nums) {
+        int size = int(nums.size());
+        vector<int> L;
+        L.reserve(size);
+        // at least one per every  increasing subsequence
+        // the longest increasing subsequence(LIS)
+        for (int i = 0; i < size; ++i) {
+            L.push_back(1);
+        }
+
+        for (int k = 0; k < size; k++) {
+            L[k] = 1 + max_sub_problems(L, nums, k);
+
+        }
+
+        return max_arr(L, size);
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    cout << solution.lengthOfLIS(nums);
+    return 0;
+}
