8705fac85faea7c243e2c8e28d0d66c60a751154
sohaib <souhaibemanah@gmail.com>
2023-10-15 22:23:39 +0100

    add

--- maze/main.py
+++ maze/main.py
@@ -1,13 +1,50 @@
-from dfs import DFS
-from pyamaze import maze, COLOR, agent
+from typing import List
+
+from bfs import BFS
+from pyamaze import maze, agent
+
+
+def isEscapePossible(blocked: List[List[int]], source: List[int], target: List[int]) -> bool:
+    """
+    :param blocked:  blocked[i] = [xi, yi] represents a blocked square with coordinates (xi, yi).
+    :param source:   [sx, sy] square
+    :param target:  target [tx, ty] square.
+    :return:
+
+    There are 1 million by 1 million grid on an XY-plane, and the coordinates of each grid square are (x, y).
+    We start at the source = and want to reach
+
+    Each move, we can walk one square north, east, south, or west if the square is not in the array of blocked squares.
+     We are also not allowed to walk outside the grid.
+
+    Return true if and only if it is possible to reach the target square from the source square through a
+    sequence of valid moves.
+
+    Constraints:
+
+    0 <= blocked.length <= 200
+    blocked[i].length == 2
+    0 <= xi, yi < 106
+    source.length == target.length == 2
+    0 <= sx, sy, tx, ty < 106
+    source != target
+    It is guaranteed that source and target are not blocked.
+    """
+    visited = [source]
+    frontier = [source]
+    while len(frontier) > 1:
+        currentNode = frontier.pop()
+        if currentNode == target:
+            return True
 
 
 def main():
-    m = maze(5, 5)
+    m = maze(50, 50)
     m.CreateMaze()
     # print(m.)
     #  1 is open, 0 is blocked
-    path = DFS(m)
+    # path = DFS(m)
+    path = BFS(m)
     a = agent(m, footprints=True)
     m.tracePath({a: path})
     m.run()
