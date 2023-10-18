81addb3ff688e5e0cc10e482b7464f3efcc12a11
sohaib <souhaibemanah@gmail.com>
2023-10-18 18:12:44 +0100

    8 puzzle

---
+++ 8-puzzle/main.py
@@ -0,0 +1,128 @@
+class Node:
+    def __init__(self, data, level, fval):
+        """ Initialize the node with the data, level of the node and the calculated fvalue """
+        self.data = data
+        self.level = level
+        self.fval = fval
+
+    def generate_child(self):
+        """ Generate child nodes from the given node by moving the blank space
+            either in the four directions {up,down,left,right} """
+        x, y = self.find(self.data, '_')
+        """ val_list contains position values for moving the blank space in either of
+            the 4 directions [up,down,left,right] respectively. """
+        val_list = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y]]
+        children = []
+        for i in val_list:
+            child = self.shuffle(self.data, x, y, i[0], i[1])
+            if child is not None:
+                child_node = Node(child, self.level + 1, 0)
+                children.append(child_node)
+        return children
+
+    def shuffle(self, puz, x1, y1, x2, y2):
+        """ Move the blank space in the given direction and if the position value is out
+            of limits the return None """
+        if 0 <= x2 < len(self.data) and 0 <= y2 < len(self.data):
+            temp_puz = self.copy(puz)
+            temp = temp_puz[x2][y2]
+            temp_puz[x2][y2] = temp_puz[x1][y1]
+            temp_puz[x1][y1] = temp
+            return temp_puz
+        else:
+            return None
+
+    @staticmethod
+    def copy(root):
+        """ Copy function to create a similar matrix of the given node"""
+        temp = []
+        for i in root:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp
+
+    def find(self, puz, x):
+        """ Specifically, used to find the position of the blank space """
+        for i in range(0, len(self.data)):
+            for j in range(0, len(self.data)):
+                if puz[i][j] == x:
+                    return i, j
+
+
+class Puzzle:
+    def __init__(self):
+        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
+        print("Enter the size of the puzzle")
+        size = int(input())
+        self.n = size
+        self.open = []
+        self.closed = []
+
+    def prompt(self):
+        """ Accepts the puzzle from the user """
+        puz = []
+        for i in range(0, self.n):
+            temp = input().split(" ")
+            puz.append(temp)
+        return puz
+
+    def f(self, start, goal):
+        """ Heuristic Function to calculate heuristic value f(x) = h(x) + g(x) """
+        return self.h(start.data, goal) + start.level
+
+    def h(self, start, goal):
+        """ Calculates the different between the given puzzles """
+        temp = 0
+        for i in range(0, self.n):
+            for j in range(0, self.n):
+                if start[i][j] != goal[i][j] and start[i][j] != '_':
+                    temp += 1
+        return temp
+
+    def process(self):
+        """ Accept Start and Goal Puzzle state"""
+        print("Enter the start state matrix \n")
+        start = self.prompt()
+        print("Enter the goal state matrix \n")
+        goal = self.prompt()
+
+        startNode = Node(start, 0, 0)
+        startNode.fval = self.f(startNode, goal)
+        """ Put the start node in the open list"""
+        self.open.append(startNode)
+        print("\n\n")
+        while True:
+            cur = self.open[0]
+            print("")
+            print("  | ")
+            print("  | ")
+            print(" \\\'/ \n")
+            for i in cur.data:
+                for j in i:
+                    print(j, end=" ")
+                print("")
+            """ If the difference between current and goal node is 0 we have reached the goal node"""
+            if self.h(cur.data, goal) == 0:
+                break
+            for i in cur.generate_child():
+                i.fval = self.f(i, goal)
+                self.open.append(i)
+            self.closed.append(cur)
+            del self.open[0]
+
+            """ sort the open list based on f value """
+            self.open.sort(key=lambda x: x.fval, reverse=False)
+
+
+def main():
+    puz = Puzzle()
+    puz.process()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
